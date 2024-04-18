b3f4c1de5dff126e4a9d02a16ece8201cb315b10
Bastien-FONTAGNE <fontagne.bastien1@gmail.com>
2024-04-18 10:51:51 +0200

    ajoute module au server.js

--- API/server.js
+++ API/server.js
@@ -1,1 +1,7 @@
 const express = require ('express');
+const mysql = require ('mysql');
+const port = 3000;
+const cors = require ('cors');
+const app = express ();
+app.use(cors());
+const sha1 = require('sha1');
